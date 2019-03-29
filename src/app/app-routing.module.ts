d721725de087c0ae9fa8fa042fcdb1feb0b289ad
APP技術部-沈哲賢 <tim.shen@mitake.com.tw>
2019-03-29 16:35:03 +0800

    實作基本導頁

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,35 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './layout/main/main.component';
 import { IndexComponent } from './layout/index/index.component';
+import { LoginComponent } from './layout/login/login.component';
+import { TermsComponent } from './layout/terms/terms.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    // loadChildren: './layout/layout.module#LayoutModule',
     children: [
       {
         path: '',
+        redirectTo: 'index',
+        pathMatch: 'full'
+      },
+      {
+        path: 'index',
         component: IndexComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'terms',
+        component: TermsComponent
       }
 
     ]
-  }
+  },
 ];
 
 @NgModule({
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,9 +5,11 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, MainComponent, IndexComponent],
+  declarations: [FooterComponent, HeaderComponent, MainComponent, IndexComponent, LoginComponent, TermsComponent],
   exports: [FooterComponent, HeaderComponent, MainComponent],
   imports: [
     CommonModule,
